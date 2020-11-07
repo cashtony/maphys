2a03fbd3ceb9199649daffd925114a2ea0ab84ae
ichitaso <ichitaso@yahoo.co.jp>
2020-11-07 09:56:43 +0900

    Support for iOS 14.2 beta 1
    using theos

--- Makefile
+++ Makefile
@@ -1,6 +1,18 @@
-.PHONY: all clean
-all:
-	xcrun -sdk iphoneos clang -arch arm64 -mios-version-min=10.0 -Weverything maphys.c -o maphys -framework IOKit -framework CoreFoundation -lcompression -O2
+DEBUG = 0
+FINALPACKAGE = 1
+GO_EASY_ON_ME := 1
+ARCHS = arm64 arm64e
 
-clean:
-	$(RM) maphys
+TARGET := iphone:clang:14.0:10.0
+MIN_IOS_SDK_VERSION = 7.0
+
+THEOS_DEVICE_IP = 0.0.0.0 -p 2222
+
+TOOL_NAME = maphys
+$(TOOL_NAME)_FILES = maphys.c
+$(TOOL_NAME)_FRAMEWORKS = IOKit CoreFoundation
+$(TOOL_NAME)_CFLAGS = -objc-arc
+$(TOOL_NAME)_CODESIGN_FLAGS = -Stfp0.plist
+
+include $(THEOS)/makefiles/common.mk
+include $(THEOS_MAKE_PATH)/tool.mk
--- maphys.c
+++ maphys.c
@@ -41,6 +41,14 @@
 #define kCFCoreFoundationVersionNumber_iOS_11_0_b1 (1429.15)
 #define kCFCoreFoundationVersionNumber_iOS_12_0_b1 (1535.13)
 #define kCFCoreFoundationVersionNumber_iOS_13_0_b1 (1652.20)
+#define kCFCoreFoundationVersionNumber_iOS_14_0_b1 (1740)
+
+#define kIODeviceTreePlane "IODeviceTree"
+#define PREBOOT_PATH "/private/preboot/"
+#define VM_KERN_MEMORY_OSKEXT (5)
+#define LOADED_KEXT_SUMMARY_HDR_NAME_OFF (0x10)
+#define LOADED_KEXT_SUMMARY_HDR_ADDR_OFF (0x60)
+#define kOSBundleLoadAddressKey "OSBundleLoadAddress"
 #define BOOT_PATH "/System/Library/Caches/com.apple.kernelcaches/kernelcache"
 
 #define DER_INT (0x2U)
@@ -92,9 +100,10 @@
 #endif
 
 typedef uint64_t kaddr_t;
+typedef char io_string_t[512];
 typedef mach_port_t io_object_t;
-typedef uint32_t ipc_entry_num_t;
-typedef io_object_t io_service_t, io_connect_t;
+typedef uint32_t IOOptionBits, ipc_entry_num_t;
+typedef io_object_t io_service_t, io_connect_t, io_registry_entry_t;
 
 typedef struct {
 	struct section_64 s64;
@@ -123,6 +132,12 @@ IOObjectRelease(io_object_t);
 CFMutableDictionaryRef
 IOServiceMatching(const char *);
 
+CFDictionaryRef
+OSKextCopyLoadedKextInfo(CFArrayRef, CFArrayRef);
+
+io_registry_entry_t
+IORegistryEntryFromPath(mach_port_t, const io_string_t);
+
 io_service_t
 IOServiceGetMatchingService(mach_port_t, CFDictionaryRef);
 
@@ -131,6 +146,9 @@ mach_vm_deallocate(vm_map_t, mach_vm_address_t, mach_vm_size_t);
 
 kern_return_t
 IOServiceOpen(io_service_t, task_port_t, uint32_t, io_connect_t *);
+
+kern_return_t
+IORegistryEntrySetCFProperty(io_registry_entry_t, CFStringRef, CFTypeRef);
 
 kern_return_t
 mach_vm_allocate(vm_map_t, mach_vm_address_t *, mach_vm_size_t, int);
@@ -143,6 +161,9 @@ mach_vm_write(vm_map_t, mach_vm_address_t, vm_offset_t, mach_msg_type_number_t);
 
 kern_return_t
 IOConnectTrap6(io_connect_t, uint32_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t);
+
+CFTypeRef
+IORegistryEntryCreateCFProperty(io_registry_entry_t, CFStringRef, CFAllocatorRef, IOOptionBits);
 
 kern_return_t
 mach_vm_read_overwrite(vm_map_t, mach_vm_address_t, mach_vm_size_t, mach_vm_address_t, mach_vm_size_t *);
@@ -580,21 +601,21 @@ pfinder_sym(pfinder_t pfinder, const char *sym) {
 	return 0;
 }
 
-static kaddr_t
-pfinder_rtclock_data(pfinder_t pfinder) {
-	kaddr_t ref = pfinder_sym(pfinder, "_RTClockData");
-	uint32_t insns[3];
-
-	if(ref != 0) {
-		return ref;
-	}
-	for(ref = pfinder_xref_str(pfinder, "assert_wait_timeout_with_leeway", 8); sec_read_buf(pfinder.sec_text, ref, insns, sizeof(insns)) == KERN_SUCCESS; ref -= sizeof(*insns)) {
-		if(IS_ADRP(insns[0]) && IS_NOP(insns[1]) && IS_LDR_W_UNSIGNED_IMM(insns[2])) {
-			return pfinder_xref_rd(pfinder, RD(insns[2]), ref, 0);
-		}
-	}
-	return 0;
-}
+//static kaddr_t
+//pfinder_rtclock_data(pfinder_t pfinder) {
+//	kaddr_t ref = pfinder_sym(pfinder, "_RTClockData");
+//	uint32_t insns[3];
+//
+//	if(ref != 0) {
+//		return ref;
+//	}
+//	for(ref = pfinder_xref_str(pfinder, "assert_wait_timeout_with_leeway", 8); sec_read_buf(pfinder.sec_text, ref, insns, sizeof(insns)) == KERN_SUCCESS; ref -= sizeof(*insns)) {
+//		if(IS_ADRP(insns[0]) && IS_NOP(insns[1]) && IS_LDR_W_UNSIGNED_IMM(insns[2])) {
+//			return pfinder_xref_rd(pfinder, RD(insns[2]), ref, 0);
+//		}
+//	}
+//	return 0;
+//}
 
 static kaddr_t
 pfinder_kernproc(pfinder_t pfinder) {
@@ -666,91 +687,145 @@ pfinder_bcopy_phys(pfinder_t pfinder) {
 
 static kaddr_t
 pfinder_init_kbase(pfinder_t *pfinder) {
-	mach_msg_type_number_t cnt = TASK_DYLD_INFO_COUNT;
-	kaddr_t addr, rtclock_data, rtclock_data_slid;
-	vm_region_extended_info_data_t extended_info;
-	task_dyld_info_data_t dyld_info;
-	struct mach_header_64 mh64;
-	mach_port_t object_name;
-	mach_vm_size_t sz;
+    mach_msg_type_number_t cnt = TASK_DYLD_INFO_COUNT;
+    vm_region_extended_info_data_t extended_info;
+    kaddr_t addr, kext_addr, kext_addr_slid;
+    CFDictionaryRef kexts_info, kext_info;
+    task_dyld_info_data_t dyld_info;
+    char kext_name[KMOD_MAX_NAME];
+    struct mach_header_64 mh64;
+    CFStringRef kext_name_cf;
+    CFNumberRef kext_addr_cf;
+    mach_port_t object_name;
+    CFArrayRef kext_names;
+    mach_vm_size_t sz;
 
-	if(task_info(tfp0, TASK_DYLD_INFO, (task_info_t)&dyld_info, &cnt) == KERN_SUCCESS) {
-		pfinder->kslide = dyld_info.all_image_info_size;
-	}
-	if(pfinder->kslide == 0 && (rtclock_data = pfinder_rtclock_data(*pfinder)) != 0) {
-		printf("rtclock_data: " KADDR_FMT "\n", rtclock_data);
-		cnt = VM_REGION_EXTENDED_INFO_COUNT;
-		for(addr = 0; mach_vm_region(tfp0, &addr, &sz, VM_REGION_EXTENDED_INFO, (vm_region_info_t)&extended_info, &cnt, &object_name) == KERN_SUCCESS; addr += sz) {
-			mach_port_deallocate(mach_task_self(), object_name);
-			if(extended_info.protection == VM_PROT_DEFAULT && extended_info.user_tag == VM_KERN_MEMORY_CPU) {
-				if(kread_addr(addr + cpu_data_rtclock_datap_off, &rtclock_data_slid) == KERN_SUCCESS && rtclock_data_slid > rtclock_data) {
-					pfinder->kslide = rtclock_data_slid - rtclock_data;
-				}
-				break;
-			}
-		}
-	}
-	if(pfinder->base + pfinder->kslide > pfinder->base && kread_buf(pfinder->base + pfinder->kslide, &mh64, sizeof(mh64)) == KERN_SUCCESS && mh64.magic == MH_MAGIC_64 && mh64.cputype == CPU_TYPE_ARM64 && mh64.filetype == MH_EXECUTE) {
-		pfinder->sec_text.s64.addr += pfinder->kslide;
-		pfinder->sec_cstring.s64.addr += pfinder->kslide;
-		printf("kbase: " KADDR_FMT ", kslide: " KADDR_FMT "\n", pfinder->base + pfinder->kslide, pfinder->kslide);
-		return KERN_SUCCESS;
-	}
-	return KERN_FAILURE;
+    if(task_info(tfp0, TASK_DYLD_INFO, (task_info_t)&dyld_info, &cnt) == KERN_SUCCESS) {
+        pfinder->kslide = dyld_info.all_image_info_size;
+    }
+    if(pfinder->kslide == 0) {
+        cnt = VM_REGION_EXTENDED_INFO_COUNT;
+        for(addr = 0; mach_vm_region(tfp0, &addr, &sz, VM_REGION_EXTENDED_INFO, (vm_region_info_t)&extended_info, &cnt, &object_name) == KERN_SUCCESS; addr += sz) {
+            mach_port_deallocate(mach_task_self(), object_name);
+            if(extended_info.protection == VM_PROT_READ && extended_info.user_tag == VM_KERN_MEMORY_OSKEXT) {
+                if(kread_buf(addr + LOADED_KEXT_SUMMARY_HDR_NAME_OFF, kext_name, sizeof(kext_name)) == KERN_SUCCESS) {
+                    printf("kext_name: %s\n", kext_name);
+                    if(kread_addr(addr + LOADED_KEXT_SUMMARY_HDR_ADDR_OFF, &kext_addr_slid) == KERN_SUCCESS) {
+                        printf("kext_addr_slid: " KADDR_FMT "\n", kext_addr_slid);
+                        if((kext_name_cf = CFStringCreateWithCStringNoCopy(kCFAllocatorDefault, kext_name, kCFStringEncodingUTF8, kCFAllocatorNull)) != NULL) {
+                            if((kext_names = CFArrayCreate(kCFAllocatorDefault, (const void **)&kext_name_cf, 1, &kCFTypeArrayCallBacks)) != NULL) {
+                                if((kexts_info = OSKextCopyLoadedKextInfo(kext_names, NULL)) != NULL) {
+                                    if(CFGetTypeID(kexts_info) == CFDictionaryGetTypeID() && CFDictionaryGetCount(kexts_info) == 1 && (kext_info = CFDictionaryGetValue(kexts_info, kext_name_cf)) != NULL && CFGetTypeID(kext_info) == CFDictionaryGetTypeID() && (kext_addr_cf = CFDictionaryGetValue(kext_info, CFSTR(kOSBundleLoadAddressKey))) != NULL && CFGetTypeID(kext_addr_cf) == CFNumberGetTypeID() && CFNumberGetValue(kext_addr_cf, kCFNumberSInt64Type, &kext_addr) && kext_addr_slid > kext_addr) {
+                                        pfinder->kslide = kext_addr_slid - kext_addr;
+                                    }
+                                    CFRelease(kexts_info);
+                                }
+                                CFRelease(kext_names);
+                            }
+                            CFRelease(kext_name_cf);
+                        }
+                    }
+                }
+                break;
+            }
+        }
+    }
+    if(pfinder->base + pfinder->kslide > pfinder->base && kread_buf(pfinder->base + pfinder->kslide, &mh64, sizeof(mh64)) == KERN_SUCCESS && mh64.magic == MH_MAGIC_64 && mh64.cputype == CPU_TYPE_ARM64 && mh64.filetype == MH_EXECUTE) {
+        pfinder->sec_text.s64.addr += pfinder->kslide;
+        pfinder->sec_cstring.s64.addr += pfinder->kslide;
+        printf("kbase: " KADDR_FMT ", kslide: " KADDR_FMT "\n", pfinder->base + pfinder->kslide, pfinder->kslide);
+        return KERN_SUCCESS;
+    }
+    return KERN_FAILURE;
+}
+
+static char *
+get_boot_path(void) {
+    size_t hash_len, path_len = sizeof(BOOT_PATH);
+    io_registry_entry_t chosen;
+    struct stat stat_buf;
+    const uint8_t *hash;
+    CFDataRef hash_cf;
+    char *path = NULL;
+
+    if(stat(PREBOOT_PATH, &stat_buf) != -1 && S_ISDIR(stat_buf.st_mode)) {
+        if((chosen = IORegistryEntryFromPath(kIOMasterPortDefault, kIODeviceTreePlane ":/chosen")) != IO_OBJECT_NULL) {
+            if((hash_cf = IORegistryEntryCreateCFProperty(chosen, CFSTR("boot-manifest-hash"), kCFAllocatorDefault, kNilOptions)) != NULL) {
+                if(CFGetTypeID(hash_cf) == CFDataGetTypeID() && (hash_len = (size_t)CFDataGetLength(hash_cf) << 1U) != 0) {
+                    path_len += strlen(PREBOOT_PATH) + hash_len;
+                    if((path = malloc(path_len)) != NULL) {
+                        memcpy(path, PREBOOT_PATH, strlen(PREBOOT_PATH));
+                        for(hash = CFDataGetBytePtr(hash_cf); hash_len-- != 0; ) {
+                            path[strlen(PREBOOT_PATH) + hash_len] = "0123456789ABCDEF"[(hash[hash_len >> 1U] >> ((~hash_len & 1U) << 2U)) & 0xFU];
+                        }
+                    }
+                }
+                CFRelease(hash_cf);
+            }
+            IOObjectRelease(chosen);
+        }
+    }
+    if(path == NULL) {
+        path_len = sizeof(BOOT_PATH);
+        path = malloc(path_len);
+    }
+    if(path != NULL) {
+        memcpy(path + (path_len - sizeof(BOOT_PATH)), BOOT_PATH, sizeof(BOOT_PATH));
+    }
+    return path;
 }
 
 static kern_return_t
 pfinder_init_offsets(void) {
-	kern_return_t ret = KERN_FAILURE;
-	pfinder_t pfinder;
+    kern_return_t ret = KERN_FAILURE;
+    pfinder_t pfinder;
+    char *boot_path;
 
-	task_map_off = 0x20;
-	proc_task_off = 0x18;
-	proc_p_pid_off = 0x10;
-	task_itk_space_off = 0x290;
-	cpu_data_rtclock_datap_off = 0x1D8;
-	vtab_get_ext_trap_for_idx_off = 0x5B8;
-	if(kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_10_0_b5) {
-		task_itk_space_off = 0x300;
-		if(kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_11_0_b1) {
-			task_itk_space_off = 0x308;
-			cpu_data_rtclock_datap_off = 0x1A8;
-			if(kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_12_0_b1) {
-				proc_task_off = 0x10;
-				proc_p_pid_off = 0x60;
-				task_itk_space_off = 0x300;
-#ifdef __arm64e__
-				cpu_data_rtclock_datap_off = 0x190;
-#else
-				cpu_data_rtclock_datap_off = 0x198;
-#endif
-				if(kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_13_0_b1) {
-					task_map_off = 0x28;
-					task_itk_space_off = 0x320;
-					vtab_get_ext_trap_for_idx_off = 0x5C0;
-					if(kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_13_0_b2) {
-						proc_p_pid_off = 0x68;
-					}
-				}
-			}
-		}
-	}
-	if(pfinder_init_file(&pfinder, BOOT_PATH) == KERN_SUCCESS) {
-		if(pfinder_init_kbase(&pfinder) == KERN_SUCCESS && (kernproc = pfinder_kernproc(pfinder)) != 0) {
-			printf("kernproc: " KADDR_FMT "\n", kernproc);
-			if((csblob_get_cdhash = pfinder_csblob_get_cdhash(pfinder)) != 0) {
-				printf("csblob_get_cdhash: " KADDR_FMT "\n", csblob_get_cdhash);
-				if((pmap_find_phys = pfinder_pmap_find_phys(pfinder)) != 0) {
-					printf("pmap_find_phys: " KADDR_FMT "\n", pmap_find_phys);
-					if((bcopy_phys = pfinder_bcopy_phys(pfinder)) != 0) {
-						printf("bcopy_phys: " KADDR_FMT "\n", bcopy_phys);
-						ret = KERN_SUCCESS;
-					}
-				}
-			}
-		}
-		pfinder_term(&pfinder);
-	}
+    proc_task_off = 0x18;
+    proc_p_pid_off = 0x10;
+    task_itk_space_off = 0x290;
+    //io_dt_nvram_of_dict_off = 0xC0;
+    if(kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_10_0_b5) {
+        task_itk_space_off = 0x300;
+        if(kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_11_0_b1) {
+            task_itk_space_off = 0x308;
+            if(kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_12_0_b1) {
+                proc_task_off = 0x10;
+                proc_p_pid_off = 0x60;
+                task_itk_space_off = 0x300;
+                if(kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_13_0_b1) {
+                    task_itk_space_off = 0x320;
+                    if(kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_13_0_b2) {
+                        proc_p_pid_off = 0x68;
+                        if(kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber_iOS_14_0_b1) {
+                            task_itk_space_off = 0x330;
+                            //io_dt_nvram_of_dict_off = 0xB8;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if((boot_path = get_boot_path()) != NULL) {
+        printf("boot_path: %s\n", boot_path);
+        if(pfinder_init_file(&pfinder, boot_path) == KERN_SUCCESS) {
+            if(pfinder_init_kbase(&pfinder) == KERN_SUCCESS && (kernproc = pfinder_kernproc(pfinder)) != 0) {
+                printf("kernproc: " KADDR_FMT "\n", kernproc);
+                if((csblob_get_cdhash = pfinder_csblob_get_cdhash(pfinder)) != 0) {
+                    printf("csblob_get_cdhash: " KADDR_FMT "\n", csblob_get_cdhash);
+                    if((pmap_find_phys = pfinder_pmap_find_phys(pfinder)) != 0) {
+                        printf("pmap_find_phys: " KADDR_FMT "\n", pmap_find_phys);
+                        if((bcopy_phys = pfinder_bcopy_phys(pfinder)) != 0) {
+                            printf("bcopy_phys: " KADDR_FMT "\n", bcopy_phys);
+                            ret = KERN_SUCCESS;
+                        }
+                    }
+                }
+            }
+            pfinder_term(&pfinder);
+        }
+        free(boot_path);
+    }
 	return ret;
 }
 
